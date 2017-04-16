72631fe2aaa89bfad808d8861226e629f2bf6242
Grafov Denis <grafov.denis@gmail.com>
2017-04-16 11:02:03 +0300

    update

--- src/transpose/Transposer.java
+++ src/transpose/Transposer.java
@@ -1,6 +1,9 @@
 package transpose;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,19 +11,26 @@ import java.util.Scanner;
 
 public class Transposer {
 
+    private String inFile;
+
+    private String outFile;
+
     private int width;
 
     private boolean alignRight;
 
     private boolean cut;
 
-    public Transposer(int width, boolean alignRight, boolean cut) {
+    public Transposer(String inFile, String outFile, int width, boolean alignRight, boolean cut) {
         this.width = width;
         this.alignRight = alignRight;
         this.cut = cut;
+        this.inFile = inFile;
+        this.outFile = outFile;
     }
 
-    public String transpose(InputStream in, OutputStream outputStream) throws IOException {
+    //доделать
+    public void transpose(InputStream in, OutputStream outputStream) throws IOException {
         try (Scanner s = new Scanner(in).useDelimiter("\\A")) {
             String lines = s.hasNext() ? s.next() : "";
             lines = lines.replace("[", "").replace("]", "");
@@ -63,14 +73,41 @@ public class Transposer {
             }
             out.close();
         }
-        return "";
     }
 
-    public String transpose(String inputName, String outputName) throws IOException {
-        try (FileInputStream inputStream = new FileInputStream(inputName)) {
-            try (FileOutputStream outputStream = new FileOutputStream(outputName)) {
-                return transpose(inputStream, outputStream);
-            }
+    //добавить выравнивание и cut
+    public void transpose(String inputName, String outputName) throws IOException {
+        String lines = Files.readAllLines(Paths.get(inputName), Charset.defaultCharset())
+                .toString().replace("[", "").replace("]", "");
+        PrintWriter out = new PrintWriter(outputName);
+
+        List<List<String>> list = new ArrayList<>();
+        for (String x : lines.split(", ")) {
+            List<String> row = new ArrayList<>();
+            Collections.addAll(row, x.split("[ ]+"));
+            list.add(row);
         }
+        int maxRowSize = 0;
+        for (int n = 0; n < list.size(); n++) {
+            if (list.get(n).size() >= maxRowSize) maxRowSize = list.get(n).size();
+        }
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < maxRowSize; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < maxRowSize; j++) {
+                try {
+                    String str = list.get(j).get(i);
+                    row.append(str).append(" ");
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+            }
+            result.add(row.toString());
+        }
+        for (int i = 0; i < result.size(); i++) {
+            out.write(result.get(i).trim());
+            if (i != result.size() - 1)
+                out.write("\n");
+        }
+        out.close();
     }
 }
--- src/transpose/TransposerLauncher.java
+++ src/transpose/TransposerLauncher.java
@@ -1,24 +1,24 @@
 package transpose;
 
-import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
-import java.io.File;
+
 import java.io.IOException;
+import java.util.Objects;
 
 public class TransposerLauncher {
-    @Option(name = "file", metaVar = "file", usage = "Задаёт имя входного файла")
+    @Option(name = "-file", metaVar = "file", usage = "Задаёт имя входного файла")
     private String inFile;
 
-    @Option(name = "-o", metaVar = "ofile", required = true, usage = "Задаёт имя выходного файла")
+    @Option(name = "-o", metaVar = "ofile", usage = "Задаёт имя выходного файла")
     private String outFile;
 
-    @Option(name = "-a", metaVar = "width", required = true, usage = "Каждое слово занимет num символов, остальное-пробелы")
+    @Option(name = "-a", metaVar = "width", usage = "Каждое слово занимет num символов, остальное-пробелы")
     private int width;
 
-    @Option(name = "-t", metaVar = "cut",  usage = "Если слово не влезает, то его обрезаем до нужного размера")
+    @Option(name = "-t", metaVar = "cut", usage = "Если слово не влезает, то его обрезаем до нужного размера")
     private boolean cut;
 
     @Option(name = "-r", usage = "Выравниваем по правой границе")
@@ -33,18 +33,19 @@ public class TransposerLauncher {
 
         try {
             parser.parseArgument(args);
-            if (width == 0 && (cut && alignRight)) width = 10;
+            if (Objects.equals(width,null) && (cut && alignRight)) width = 10;
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println("java - jar transpose.jar -a cut -t cut -r -o ofile file");
+            System.err.println("java - jar transpose.jar -a width -t cut -r -o ofile -file");
             parser.printUsage(System.err);
             return;
         }
-        Transposer transpose = new Transposer(width,alignRight,cut);
+        Transposer transpose = new Transposer(inFile, outFile, width, alignRight, cut);
         try {
-            transpose.transpose(inFile,outFile);
+            if (inFile != null)
+            transpose.transpose(inFile, outFile);
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            System.err.print(e.getMessage());
         }
     }
 }
